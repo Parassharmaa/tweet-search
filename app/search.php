0589e646babe244f8ed839b40dd7d2665a414a2a
paras <parazz@outlook.com>
2017-02-26 13:12:17 +0530

    Minor change

--- app/search.php
+++ app/search.php
@@ -5,10 +5,10 @@ if(isset($_GET['query'])) {
 	require('class/TwitterAPIExchange.php');
 
 	$settings = array(
-    'oauth_access_token' => "850697540-ZJcAlOKgCCOCtY1QH5ka9QItkEWejLuwmcA2SyCP",
-    'oauth_access_token_secret' => "P7gXcpw4lpCxKJhTMlrrH0I7X2DC3vf4R1nKzkOyMKmYA",
-    'consumer_key' => "zSVvysItsAAOrVFO1gJROryxm",
-    'consumer_secret' => "PaBMpshMfcDd6CDCQSPTLMvZXveNz5NaXS0MWrmB3jRfokS3JW"
+    'oauth_access_token' => "xxxx",
+    'oauth_access_token_secret' => "xxxx",
+    'consumer_key' => "xxx",
+    'consumer_secret' => "xxxx"
 );
 	$url = 'https://api.twitter.com/1.1/search/tweets.json';
 	$getfield = "?q=".$qu."&lang=en&count=10";
